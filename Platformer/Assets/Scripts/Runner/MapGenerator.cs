ce325a6aeff8a8793f7ce4b7c650ca857fd73443
Aleksbgbg <kozichka01@abv.bg>
2018-11-22 20:40:54 +0000

    [Runner] Add script and scene to generate a random level when the scene is loaded.

---
+++ Platformer/Assets/Scripts/Runner/MapGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace Scripts.Runner
+{
+    using UnityEngine;
+
+    public class MapGenerator : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform[] _levels;
+
+        private void Awake()
+        {
+            Instantiate(_levels[Random.Range(0, _levels.Length)]);
+        }
+    }
+}
