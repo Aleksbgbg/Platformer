c21b510fbadf0c1d3d306385301d7c91ccd6df30
Aleksbgbg <kozichka01@abv.bg>
2018-11-22 09:17:31 +0000

    [Animations] Add hurt animation on player death.

--- Platformer/Assets/Scripts/Player/PlayerEnemyCollision.cs
+++ Platformer/Assets/Scripts/Player/PlayerEnemyCollision.cs
@@ -4,21 +4,31 @@
 
     public class PlayerEnemyCollision : MonoBehaviour
     {
+        private Animator _animator;
+
         private PlayerController _playerController;
 
         private Rigidbody2D _rigidBody;
 
         private void Awake()
         {
+            _animator = GetComponent<Animator>();
             _playerController = gameObject.GetComponent<PlayerController>();
             _rigidBody = gameObject.GetComponent<Rigidbody2D>();
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            if (collision.relativeVelocity.y > 0.0f && collision.gameObject.CompareTag("Enemy"))
+            if (collision.gameObject.CompareTag("Enemy"))
             {
-                _rigidBody.AddForce(new Vector2(0.0f, _playerController.JumpForce));
+                if (collision.relativeVelocity.y > 0.0f)
+                {
+                    _rigidBody.AddForce(new Vector2(0.0f, _playerController.JumpForce));
+                }
+                else
+                {
+                    _animator.SetTrigger("IsDead");
+                }
             }
         }
     }
