8c05b71fd3ad99b8e16c051b15e5e3421cb630c5
Aleksbgbg <kozichka01@abv.bg>
2018-11-22 22:03:50 +0000

    [MapGenerator] Add generator which pieces together runner map components to form an infinite map.

---
+++ Platformer/Assets/Scripts/Runner/Level.cs
@@ -0,0 +1,38 @@
+﻿namespace Scripts.Runner
+{
+    using UnityEngine;
+    using UnityEngine.Tilemaps;
+
+    internal class Level
+    {
+        private readonly Transform _transform;
+
+        private readonly Tilemap _tilemap;
+
+        private readonly TilemapRenderer _tilemapRenderer;
+
+        internal Level(Transform transform)
+        {
+            _transform = transform;
+
+            Grid grid = transform.GetComponent<Grid>();
+
+            _tilemap = grid.GetComponentInChildren<Tilemap>();
+            _tilemapRenderer = grid.GetComponentInChildren<TilemapRenderer>();
+        }
+
+        internal bool IsVisible => _tilemapRenderer.isVisible;
+
+        internal float Size => _tilemap.size.x;
+
+        internal void Destroy()
+        {
+            Object.Destroy(_transform.gameObject);
+        }
+
+        internal bool IsNearingEnd(Camera camera)
+        {
+            return camera.WorldToScreenPoint(new Vector3(_transform.position.x + (Size / 2.0f), 0.0f)).x <= camera.pixelWidth;
+        }
+    }
+}
--- Platformer/Assets/Scripts/Runner/MapGenerator.cs
+++ Platformer/Assets/Scripts/Runner/MapGenerator.cs
@@ -1,15 +1,57 @@
 ﻿namespace Scripts.Runner
 {
+    using System.Collections.Generic;
+
     using UnityEngine;
 
     public class MapGenerator : MonoBehaviour
     {
+        private readonly Queue<Level> _levelQueue = new Queue<Level>();
+
+        [SerializeField]
+        private Camera _mainCamera;
+
         [SerializeField]
         private Transform[] _levels;
 
+        private float _spawnPosition;
+
+        private Level _activeLevel;
+
+        private void ActivateLevel()
+        {
+            ActivateLevel(Random.Range(0, _levels.Length));
+        }
+
+        private void ActivateLevel(int index)
+        {
+            Transform activeLevelTransform = Instantiate(_levels[index], new Vector3(_spawnPosition, 0.0f, 0.0f), new Quaternion());
+
+            Level activeLevel = new Level(activeLevelTransform);
+
+            _levelQueue.Enqueue(activeLevel);
+            _activeLevel = activeLevel;
+
+            _spawnPosition += activeLevel.Size;
+        }
+
         private void Awake()
         {
-            Instantiate(_levels[Random.Range(0, _levels.Length)]);
+            ActivateLevel(1);
+        }
+
+        private void Update()
+        {
+            if (_levelQueue.Count > 1 &&
+                !_levelQueue.Peek().IsVisible)
+            {
+                _levelQueue.Dequeue().Destroy();
+            }
+
+            if (_activeLevel.IsNearingEnd(_mainCamera))
+            {
+                ActivateLevel();
+            }
         }
     }
 }
